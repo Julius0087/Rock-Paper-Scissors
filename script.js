1aa3ec4dba38a842d76fa10a8609597efcc9daa4
Julius <123553495+Julius0087@users.noreply.github.com>
2023-05-17 18:49:29 +0200

    Rock paper scissors UI
    - remove the prompt option - game is now playable using buttons
    - add a ui to the RPS game
    - add replayability
    - add CSS with header, body content and footer

--- script.js
+++ script.js
@@ -1,50 +1,89 @@
 let playerScore = 0
 let computerScore = 0
 
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+
+const resultSpan = document.querySelector("span#result");
+const playerScoreSpan = document.querySelector("span#player-score");
+const computerScoreSpan = document.querySelector("span#computer-score");
+const resultHeading = document.querySelector("#result-heading");
+const resultsDiv = document.querySelector(".results");
+
+// creates the "play again" button in memory, doesn't render it yet
+const again = document.createElement("button");
+again.classList.add("again-button");
+again.textContent = "Play again";
+
+// function that executes one game
+function game() {
+  rockButton.addEventListener("click", oneRound);
+  paperButton.addEventListener("click", oneRound);
+  scissorsButton.addEventListener("click", oneRound);
+
+  playerScore = 0;
+  computerScore = 0;
+
+  resultSpan.textContent = "";
+  playerScoreSpan.textContent = "Player score: 0";
+  computerScoreSpan.textContent = "Computer score: 0";
+  resultHeading.textContent = "";
+  resultsDiv.removeChild(again);
+}
+game();
+
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * (3 - 1 + 1) ) + 1
-  if (choice === 1) return "rock"
-  else if (choice === 2) return "paper"
-  else return "scissors"
+  if (choice === 1) return "rock";
+  else if (choice === 2) return "paper";
+  else return "scissors";
 }
 
 function playRound(playerSelection, computerSelection) {
-  
   if (playerSelection === computerSelection) {
-    return "It's a tie!"
+    resultSpan.textContent = "It's a tie!";
   }
   else if ((playerSelection === "rock" && computerSelection === "paper") || 
   (playerSelection === "paper" && computerSelection === "scissors") ||
   (playerSelection === "scissors" && computerSelection === "rock")) {
-    computerScore++
-    return `You lose! ${computerSelection} beats ${playerSelection}!`
+    computerScore++;
+    resultSpan.textContent = `You lose! ${computerSelection} beats ${playerSelection}!`;
   }
   else {
     playerScore++
-    return `You win! ${playerSelection} beats ${computerSelection}!`
+    resultSpan.textContent = `You win! ${playerSelection} beats ${computerSelection}!`;
   }
 }
 
-function checkForSymbol(playerSelection) {
-  playerSelection = playerSelection.toLowerCase()
-  if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") return true
-  else false
+// updates the score, stops the game if neccessary
+function updateScore() {
+  playerScoreSpan.textContent = `Player score: ${playerScore}`;
+  computerScoreSpan.textContent = `Computer score: ${computerScore}`;
+
+  if (playerScore === 5) {
+    resultHeading.textContent = "The player has won!";
+    stopGame();
+  }
+  if (computerScore === 5) {
+    resultHeading.textContent = "The computer has won!";
+    stopGame();
+  }
 }
 
-function game() {
-  for (let i = 0; i < 5; i ++) {
-    let playerSelection
-    while (true) {
-      playerSelection = prompt("Select a symbol")
-      if (checkForSymbol(playerSelection)) break
-    }
-      
-    const computerSelection = getComputerChoice()
-    let result = playRound(playerSelection, computerSelection)
-    console.log(result)
-  }
-  if (playerScore > computerScore) console.log("The player has won!")
-  else if (computerScore > playerScore) console.log("The computer has won!")
-  else console.log("The match is a tie!")
+// removes the event listeners, renders the "play again" button - if clicked, new game begins
+function stopGame() {
+  rockButton.removeEventListener("click", oneRound);
+  paperButton.removeEventListener("click", oneRound);
+  scissorsButton.removeEventListener("click", oneRound);
+  
+  resultsDiv.appendChild(again);
+
+  again.addEventListener("click", game)
 }
-game()
+
+function oneRound(e) {
+  symbol = e.target.getAttribute("id")
+  playRound(symbol, getComputerChoice());
+  updateScore();
+}
